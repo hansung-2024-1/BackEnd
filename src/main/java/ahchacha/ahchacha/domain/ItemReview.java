a75ba42fa4a47e585eeaee600bff8c5d600e06c7
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-26 20:31:08 +0900

    [FEAT] ItemReview 도메인 생성

---
+++ src/main/java/ahchacha/ahchacha/domain/ItemReview.java
@@ -0,0 +1,39 @@
+package ahchacha.ahchacha.domain;
+
+import ahchacha.ahchacha.domain.common.BaseEntity;
+import ahchacha.ahchacha.domain.common.enums.PersonType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ItemReview extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String reviewComment;
+
+    //precision : 소수점을 포함한 전체 자릿수 / scale : 소수점 이하의 자릿수
+    @Column(nullable = false, precision = 2, scale = 1)
+    private BigDecimal reviewScore;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PersonType personType;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Item item;
+}
