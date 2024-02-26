d89fa7d163e85355e1dfebe5854d57e9f0dc83ba
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-26 16:46:25 +0900

    initialize project

---
+++ src/main/java/ahchacha/ahchacha/AhchachaApplication.java
@@ -0,0 +1,13 @@
+package ahchacha.ahchacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhchachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhchachaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ahchacha/ahchacha/AhchachaApplicationTests.java
@@ -0,0 +1,13 @@
+package ahchacha.ahchacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhchachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
