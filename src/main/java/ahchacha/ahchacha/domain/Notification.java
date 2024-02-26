b09856578623a92fd8674c6b1c9fba980a194b4a
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-26 20:30:57 +0900

    [FEAT] Notification 도메인 생성

---
+++ src/main/java/ahchacha/ahchacha/domain/Notification.java
@@ -0,0 +1,26 @@
+package ahchacha.ahchacha.domain;
+
+import ahchacha.ahchacha.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Notification extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    private boolean isRead;
+}
