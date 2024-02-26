b5d4af513a9d4623fbaf69e45d55c10feb436eed
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-26 20:30:44 +0900

    [FEAT] User 도메인 생성

---
+++ src/main/java/ahchacha/ahchacha/domain/User.java
@@ -0,0 +1,31 @@
+package ahchacha.ahchacha.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Column(nullable = false, length = 15)
+    private String nickname;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Notification> notifications = new ArrayList<>();
+}
