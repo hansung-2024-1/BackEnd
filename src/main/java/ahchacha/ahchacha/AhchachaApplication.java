e70762559eae6aa1bec0e1a8ea3f6997d66e4184
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-03-08 16:27:56 +0900

    [ADD] createdAt, updatedAt 추가하기 위한 어노테이션 추가

--- src/main/java/ahchacha/ahchacha/AhchachaApplication.java
+++ src/main/java/ahchacha/ahchacha/AhchachaApplication.java
@@ -2,8 +2,10 @@ package ahchacha.ahchacha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AhchachaApplication {
 
 	public static void main(String[] args) {
