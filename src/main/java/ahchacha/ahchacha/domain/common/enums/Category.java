ec3ae97d1319c4ed630ff43d6b409957d4cb2d50
kwon2059 <tmdwls2059@naver.com>
2024-03-26 19:38:01 +0900

    [ADD] 카테고리 추가

--- src/main/java/ahchacha/ahchacha/domain/common/enums/Category.java
+++ src/main/java/ahchacha/ahchacha/domain/common/enums/Category.java
@@ -1,9 +1,14 @@
 package ahchacha.ahchacha.domain.common.enums;
 
 public enum Category {
-    // 1차 카테고리
-    ELECTRONICDEVICE, SPORT, BEAUTY, LIFE, BOOK, // 전자기기 ,스포츠용품 , 미용&뷰티 , 생활 , 책
-
-    //2차 카테고리
-    CHARGER, SUPPLEMENTARYBATTERY, LAPTOP //충전기, 보조배터리, 노트북
+    // 전자기기
+    노트북, 마우스, 충전기, 보조배터리, 블루투스스피커, 키보드, 카메라, 케이블, 계산기,
+    //생활
+    책, 커피포트, 보드게임, 퍼즐, 카드, 우산, 돗자리,
+    //헬스
+    운동기기, 공, 운동화, 덤벨, 매트, 푸쉬업바, 풀업바,
+    //예술
+    음악, 악기,
+    //기타
+    기타
 }
